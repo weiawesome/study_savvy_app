52c45ccbc25cfba03a28367fa94918d883c67f2a
tcweeei <open891013@gmail.com>
2023-07-01 14:00:06 +0800

    Complete Photo load.

--- lib/screens/specific_file.dart
+++ lib/screens/specific_file.dart
@@ -1,8 +1,8 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:study_savvy_app/blocs/bloc_jwt.dart';
 import 'package:study_savvy_app/models/model_files.dart';
-import 'package:study_savvy_app/services/jwt_storage.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:http/http.dart' as http;
 class SpecificFilePage extends StatefulWidget{
   final File_Info fileInfo;
   const SpecificFilePage({Key?key,required this.fileInfo}):super(key: key);
@@ -12,6 +12,18 @@ class SpecificFilePage extends StatefulWidget{
 
 class _SpecificFilePage extends State<SpecificFilePage> {
   final Specific_File specificFile=Specific_File("Content","prompt","Summarize-----\n\n\nbflajkbedlj\n-",["details","details"]);
+
+  Future<Uint8List> _downloadImage(String url, String jwtToken) async {
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {'Authorization': 'Bearer $jwtToken'},
+    );
+    if (response.statusCode == 200) {
+      return response.bodyBytes;
+    } else {
+      throw Exception('Failed to load image');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -116,7 +128,24 @@ class _SpecificFilePage extends State<SpecificFilePage> {
                                   ],
                                 ),
                               ),
-                              this.widget.fileInfo.type=='ASR'?Container(color: Colors.red,height: 10,):Container(color:Colors.green,height: 10,)
+                              this.widget.fileInfo.type=='ASR'?
+                              Container(
+                                color: Colors.black,
+                                height: 200,
+                                child: FutureBuilder<Uint8List>(
+                                  future: _downloadImage('https://study-savvy.com/api/files/resources/graph/2e540dc9-3b25-4f51-b5b2-3583fabc2e53', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY4ODE4MTk1NywianRpIjoiNDdlMDA1OWMtNDFjOS00YjdjLTk3MWUtMTJiZmU2NDJiYTg5IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IndlaTg5MTAxM0BnbWFpbC5jb20iLCJuYmYiOjE2ODgxODE5NTcsImNzcmYiOiI4N2I1Y2Q2OS0xOWQ3LTQ2MjMtOGNmMi1jYjdmZmZjYmI1MWIiLCJleHAiOjE2ODkzOTE1NTd9.jNmZ69n9F_N0mvTl8_DSHoymZMka33goA7D1RBHq2Hw'),
+                                  builder: (BuildContext context, AsyncSnapshot<Uint8List> snapshot) {
+                                    if (snapshot.connectionState == ConnectionState.waiting) {
+                                      return CircularProgressIndicator();
+                                    } else if (snapshot.hasError) {
+                                      return Text('Error: ${snapshot.error}');
+                                    } else {
+                                      return Image.memory(snapshot.data!);
+                                    }
+                                  },
+                                ),
+                              ):
+                              Container(color:Colors.green,height: 10,)
                             ],
                           )
                       ),
@@ -133,7 +162,7 @@ class _SpecificFilePage extends State<SpecificFilePage> {
                             style: Theme.of(context).elevatedButtonTheme.style,
                           ),
                           ElevatedButton(
-                            onPressed: () { Navigator.pop(context); },
+                            onPressed: () async { await DefaultCacheManager().emptyCache(); },
                             child:Text("Delete",textAlign: TextAlign.center,style: TextStyle(color: Colors.redAccent, fontSize:23,fontFamily: 'Play',fontWeight: FontWeight.bold),),
                             style: Theme.of(context).elevatedButtonTheme.style,
                           )
