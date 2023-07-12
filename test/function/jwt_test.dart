0e9529f61fbf091120cad863722df1f73ebcf494
tcweeei <open891013@gmail.com>
2023-07-12 22:51:57 +0800

    Jwt-storage test.

--- test/function/jwt_test.dart
+++ test/function/jwt_test.dart
@@ -9,11 +9,54 @@ import 'jwt_test.mocks.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   group('JwtService', () {
-    test('saveJwt saves jwt', () async {
+    test('JwtService saveJwt test', () async {
       MockFlutterSecureStorage storage=MockFlutterSecureStorage();
       when(storage.write(key: anyNamed('key'), value: anyNamed('value'))).thenAnswer((_) => Future.value());
-      await JwtService.saveJwt('test_jwt');
+      JwtService jwtService=JwtService(storage: storage);
+      await jwtService.saveJwt('test_jwt');
       verify(storage.write(key: 'jwt', value: 'test_jwt')).called(1);
     });
+    test('JwtService getJwt test', () async {
+      MockFlutterSecureStorage storage=MockFlutterSecureStorage();
+      when(storage.read(key: anyNamed('key'))).thenAnswer((_) => Future.value());
+      JwtService jwtService=JwtService(storage: storage);
+      await jwtService.getJwt();
+      verify(storage.read(key: 'jwt')).called(1);
+    });
+    test('JwtService deleteJwt test', () async {
+      MockFlutterSecureStorage storage=MockFlutterSecureStorage();
+      when(storage.delete(key: anyNamed('key'))).thenAnswer((_) => Future.value());
+      JwtService jwtService=JwtService(storage: storage);
+      await jwtService.deleteJwt();
+      verify(storage.delete(key: 'jwt')).called(1);
+    });
+    test('JwtService hasJwt returns true when jwt exists', () async {
+      final storage = MockFlutterSecureStorage();
+
+      when(storage.containsKey(key: anyNamed('key'))).thenAnswer((_) => Future.value(true));
+
+      final jwtService = JwtService(storage: storage);
+
+      bool? result = await jwtService.hasJwt();
+
+      verify(storage.containsKey(key: 'jwt')).called(1);
+
+      expect(result, isTrue);
+    });
+
+    test('JwtService hasJwt returns false when jwt does not exist', () async {
+      final storage = MockFlutterSecureStorage();
+
+      when(storage.containsKey(key: anyNamed('key'))).thenAnswer((_) => Future.value(false));
+
+      final jwtService = JwtService(storage: storage);
+
+      bool? result = await jwtService.hasJwt();
+
+      verify(storage.containsKey(key: 'jwt')).called(1);
+
+      expect(result, isFalse);
+    });
+
   });
 }
