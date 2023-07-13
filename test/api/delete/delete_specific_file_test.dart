5eae2a85f79329072e265265096dfa793f2dd3b8
tcweeei <open891013@gmail.com>
2023-07-13 14:47:33 +0800

    Delete specific-file test.

--- test/api/delete/delete_specific_file_test.dart
+++ test/api/delete/delete_specific_file_test.dart
@@ -1,25 +1,150 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:study_savvy_app/services/api_files.dart';
 import 'package:study_savvy_app/services/api_routes.dart';
+import 'package:study_savvy_app/services/jwt_storage.dart';
+import 'package:study_savvy_app/utils/exception.dart';
 import '../api_test.mocks.dart';
 import 'package:http/http.dart' as http;
 
+@GenerateMocks([JwtService, http.Client])
 void main() {
-  final jwtService = MockJwtService();
-  final httpClient = MockClient();
-
   test('Fetch specific file', () async {
+    final jwtService = MockJwtService();
+    final httpClient = MockClient();
+
     const String testId="Test id";
     const String testJwt="Test jwt";
-    final url = Uri.parse("${ApiRoutes.fileUrl}$testId");
+
+    final url = Uri.parse("${ApiRoutes.fileUrl}/$testId");
     when(jwtService.getJwt()).thenAnswer((_) async => testJwt);
-    when(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).thenAnswer(
-          (_) async => http.Response('', 201),
-    );
+    when(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).thenAnswer((_) async => http.Response('', 201),);
 
     final FilesService apiService = FilesService(jwtService: jwtService, httpClient: httpClient);
     await apiService.deleteSpecificFile(testId);
 
+    verify(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).called(1);
+
   });
+  test('deleteSpecificFile throws ClientException', () async {
+    final jwtService = MockJwtService();
+    final httpClient = MockClient();
+
+    const String testId="Test id";
+    const String testJwt="Test jwt";
+
+    final url = Uri.parse("${ApiRoutes.fileUrl}/$testId");
+    when(jwtService.getJwt()).thenAnswer((_) async => testJwt);
+    when(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).thenAnswer((_) async => http.Response('', 400),);
+
+    final FilesService apiService = FilesService(jwtService: jwtService, httpClient: httpClient);
+
+    expectLater(apiService.deleteSpecificFile(testId), throwsA(isA<ClientException>()));
+
+    await untilCalled(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'}));
+    verify(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).called(1);
+
+  });
+  test('deleteSpecificFile throws ExistException', () async {
+    final jwtService = MockJwtService();
+    final httpClient = MockClient();
+
+    const String testId="Test id";
+    const String testJwt="Test jwt";
+
+    final url = Uri.parse("${ApiRoutes.fileUrl}/$testId");
+    when(jwtService.getJwt()).thenAnswer((_) async => testJwt);
+    when(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).thenAnswer((_) async => http.Response('', 404),);
+
+    final FilesService apiService = FilesService(jwtService: jwtService, httpClient: httpClient);
+
+    expectLater(apiService.deleteSpecificFile(testId), throwsA(isA<ExistException>()));
+
+    await untilCalled(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'}));
+    verify(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).called(1);
+
+  });
+  test('deleteSpecificFile throws AuthException', () async {
+    final jwtService = MockJwtService();
+    final httpClient = MockClient();
+
+    const String testId="Test id";
+    const String testJwt="Test jwt";
+
+    final url = Uri.parse("${ApiRoutes.fileUrl}/$testId");
+    when(jwtService.getJwt()).thenAnswer((_) async => testJwt);
+    when(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).thenAnswer((_) async => http.Response('', 422),);
+
+    final FilesService apiService = FilesService(jwtService: jwtService, httpClient: httpClient);
+
+    expectLater(apiService.deleteSpecificFile(testId), throwsA(isA<AuthException>()));
+
+    await untilCalled(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'}));
+    verify(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).called(1);
+
+    await untilCalled(jwtService.deleteJwt());
+    verify(jwtService.deleteJwt()).called(1);
+
+  });
+  test('deleteSpecificFile throws ServerException', () async {
+    final jwtService = MockJwtService();
+    final httpClient = MockClient();
+
+    const String testId="Test id";
+    const String testJwt="Test jwt";
+
+    final url = Uri.parse("${ApiRoutes.fileUrl}/$testId");
+    when(jwtService.getJwt()).thenAnswer((_) async => testJwt);
+    when(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).thenAnswer(
+          (_) async => http.Response('', 500),
+    );
+    final FilesService apiService = FilesService(jwtService: jwtService, httpClient: httpClient);
+
+    expectLater(apiService.deleteSpecificFile(testId), throwsA(isA<ServerException>()));
+
+    await untilCalled(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'}));
+    verify(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).called(1);
+
+  });
+
+  test('deleteSpecificFile throws Other Exception', () async {
+    final jwtService = MockJwtService();
+    final httpClient = MockClient();
+
+    const String testId="Test id";
+    const String testJwt="Test jwt";
+
+    final url = Uri.parse("${ApiRoutes.fileUrl}/$testId");
+    when(jwtService.getJwt()).thenAnswer((_) async => testJwt);
+    when(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).thenAnswer(
+          (_) async => http.Response('', 555),
+    );
+    final FilesService apiService = FilesService(jwtService: jwtService, httpClient: httpClient);
+
+    expectLater(apiService.deleteSpecificFile(testId), throwsA(isA<Exception>()));
+
+    await untilCalled(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'}));
+    verify(httpClient.delete(url, headers: {'Authorization': 'Bearer $testJwt'})).called(1);
+
+  });
+
+  // test('deleteSpecificFile throws Other Exception(Jwt is null)', () async {
+  //   final jwtService = MockJwtService();
+  //   final httpClient = MockClient();
+  //
+  //   const String testId="Test id";
+  //
+  //   final url = Uri.parse("${ApiRoutes.fileUrl}/$testId");
+  //   when(jwtService.getJwt()).thenAnswer((_) async => null);
+  //   when(httpClient.delete(url, headers: {'Authorization': 'Bearer ${jwtService.getJwt()}'})).thenAnswer(
+  //         (_) async => http.Response('', 555),
+  //   );
+  //   final FilesService apiService = FilesService(jwtService: jwtService, httpClient: httpClient);
+  //
+  //   await apiService.deleteSpecificFile(testId);
+  //   expect(apiService.deleteSpecificFile(testId), throwsA(isA<Exception>()));
+  //
+  // });
+
 }
