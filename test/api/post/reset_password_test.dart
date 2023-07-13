78e0c2d656eefcd8b1939d254f1746a7c8728bbb
tcweeei <open891013@gmail.com>
2023-07-13 23:14:16 +0800

    Test of method of put of reset-password.

--- test/api/post/reset_password_test.dart
+++
@@ -1,6 +0,0 @@
-import 'package:mockito/annotations.dart';
-import 'package:http/http.dart' as http;
-import 'package:study_savvy_app/services/jwt_storage.dart';
-
-@GenerateMocks([JwtService, http.Client])
-void main() {}
