9f6562b388697f88d21e94183857e1baaec31545
tcweeei <open891013@gmail.com>
2023-07-14 11:58:01 +0800

    Try to do bloc unit test.

--- test/bloc/bloc_access_methods_test.dart
+++ test/bloc/bloc_access_methods_test.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -7,34 +8,26 @@ import 'bloc_access_methods_test.mocks.dart';
 
 @GenerateMocks([ProfileService])
 void main() {
-  // group('AccessMethodBloc', () {
-  //   late AccessMethodBloc accessMethodBloc;
-  //   late MockProfileService mockProfileService;
-  //
-  //   setUp(() {
-  //     mockProfileService = MockProfileService();
-  //     accessMethodBloc = AccessMethodBloc(profileService: mockProfileService);
-  //   });
-  //
-  //   tearDown(() {
-  //     accessMethodBloc.close();
-  //   });
-  //
-  //   test('emits [PENDING, SUCCESS] when AccessMethodEventApiKey succeeded', () async {
-  //     const expectedResponse = 'success';
-  //     when(mockProfileService.setApiKey(any)).thenAnswer((_) async => expectedResponse);
-  //
-  //     final expectedStates = [
-  //       AccessMethodState("PENDING", null),
-  //       AccessMethodState("SUCCESS", null),
-  //     ];
-  //
-  //     expectLater(
-  //       accessMethodBloc.stream,
-  //       emitsInOrder(expectedStates),
-  //     );
-  //
-  //     accessMethodBloc.add(AccessMethodEventApiKey('apiKey'));
-  //   });
-  // });
+  group('AccessMethodBloc', () {
+    test('emits [PENDING, SUCCESS] when AccessMethodEventApiKey succeeded', () async {
+      final MockProfileService mockProfileService=MockProfileService();
+      final AccessMethodBloc accessMethodBloc=AccessMethodBloc(apiService: mockProfileService);
+
+      when(mockProfileService.setApiKey(any)).thenAnswer((_) async => '');
+
+      final expectedStates = [
+        AccessMethodState("PENDING", null),
+        AccessMethodState("SUCCESS", null),
+      ];
+
+      accessMethodBloc.add(AccessMethodEventApiKey('apiKey'));
+      await expectLater(
+        accessMethodBloc.stream.map((event) => event),
+        emitsInOrder(expectedStates),
+      );
+
+      // await untilCalled(mockProfileService.setApiKey(any));
+      // verify(mockProfileService.setApiKey(any)).called(1);
+    });
+  });
 }
