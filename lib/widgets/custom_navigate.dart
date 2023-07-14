f0c7fa111d66951d8d8e83dc8f1c7cc2be7cc98c
tcweeei <open891013@gmail.com>
2023-07-14 15:11:16 +0800

    Edit bottom-navigator UI.

--- lib/widgets/custom_navigate.dart
+++ lib/widgets/custom_navigate.dart
@@ -12,36 +12,35 @@ class CustomNavigate extends StatefulWidget {
 class _CustomNavigate extends State<CustomNavigate> {
   @override
   Widget build(BuildContext context) {
+    const double iconSize=35;
     return BottomNavigationBar(
-
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
-          icon: Icon(Icons.feed_outlined,size: 38,),
-          activeIcon: Icon(Icons.feed_outlined,size: 38,),
+          icon: Icon(Icons.file_open_outlined,size: iconSize,),
+          activeIcon: Icon(Icons.file_open_outlined,size: iconSize,),
           label: 'Note Maker',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.mode_edit_outline_outlined,size: 38),
-          activeIcon: Icon(Icons.mode_edit_outline_outlined,size: 38),
+          icon: Icon(Icons.mode_edit_outline_outlined,size: iconSize),
+          activeIcon: Icon(Icons.mode_edit_outline_outlined,size: iconSize),
           label: 'Writing Improver',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.access_time,size: 38),
-          activeIcon: Icon(Icons.access_time,size: 38),
+          icon: Icon(Icons.access_time,size: iconSize),
+          activeIcon: Icon(Icons.access_time,size: iconSize),
           label: 'Files',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.person,size: 38),
-          activeIcon: Icon(Icons.person,size: 38),
+          icon: Icon(Icons.person,size: iconSize),
+          activeIcon: Icon(Icons.person,size: iconSize),
           label: 'Profile',
         ),
       ],
-
       backgroundColor: Theme.of(context).primaryColor,
       showSelectedLabels: false,
       showUnselectedLabels: false,
       selectedItemColor: Theme.of(context).brightness== Brightness.dark?const Color.fromRGBO(217,217,217,1):Colors.black,
-      unselectedItemColor: const Color.fromRGBO(118,118,118,1),
+      unselectedItemColor: const Color.fromRGBO(118,118,118,0.4),
       onTap: (index) {
         context.read<PageBloc>().add(pageEvents[index]);
       },
